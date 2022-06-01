b6aa425a7ab709f4cd1b109c66bf4923dd179f87
Aditya Kakad <202052302@iiitvadodara.ac.in>
2022-06-02 00:47:48 +0530

    final touches

---
+++ js/script.js
@@ -0,0 +1,12 @@
+var typed = new Typed(".typing",{
+    strings:[
+        " ",
+        "Full Stack Developer",
+        "Web Designer",
+        "Java Programmer",
+        "Blockchain Enthusiast"
+],
+typeSpeed:100,
+BackSpeed:500,
+loop:true
+});
