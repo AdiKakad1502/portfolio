6e25d8587f8d19ce9b9b1c077f6b72f90f52af6e
Aditya Kakad <202052302@iiitvadodara.ac.in>
2023-03-16 23:03:16 +0530

    uefv

--- js/script.js
+++ js/script.js
@@ -10,3 +10,16 @@ typeSpeed:100,
 BackSpeed:500,
 loop:true
 });
+
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav");
+
+hamburger.addEventListener("click",()=>{
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+})
+
+document.querySelectorAll(".nav-link").forEach(n=> n.addEventListener("click",() => {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+}))
